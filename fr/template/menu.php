56a7a6b4ae96ec58592fa0bc66521b1d33fd95f4
William PENSEC <william.pensec@univ-ubs.fr>
2024-05-30 16:12:27 +0200

    MaJ index.php avec carte et automatisation google scholar + bootstrap 5

--- fr/template/menu.php
+++ fr/template/menu.php
@@ -8,40 +8,37 @@ $newPageEn = ".." . preg_replace("/(\/fr)/i", "/en", $page);
 $newPageIt = ".." . preg_replace("/(\/fr)/i", "/it", $page);
 ?>
 
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top ps-4 align-items-bottom">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
         <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="navbar-brand" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/index.php"?>" style="color:white">Accueil</a>
+            <li class="navbar-brand">
+                <img src="<?php echo $_SESSION['baseURL'] . "logo/favicon-32x32.png" ?>" class="rounded-circle" alt="Cinque Terre">
             </li>
-            <li class="nav-item">
-                <a class="navbar-brand" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/cv.php"?>" style="color:white">CV</a>
+            <li class="navbar-brand">
+                <a href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/index.php" ?>" style="color:white"><i class="bi bi-house me-2"></i>Accueil</a>
             </li>
-            <!-- <li class="nav-item">
-                <a class="navbar-brand" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/projets.php"?>" style="color:white">Projets</a>
-            </li> -->
+            <li class="navbar-brand">
+                <a href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/cv.php" ?>" style="color:white">CV</a>
+            </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/" . $currPage?>" id="engagement" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white">Engagement scientifique et universitaire</a>
+                <a class="nav-link dropdown-toggle navbar-brand" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/" . $currPage ?>" id="engagement" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white">Engagement scientifique et universitaire</a>
                 <div class="dropdown-menu" aria-labelledby="engagement">
-                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/enseignements.php"?>"><i class="fas fa-chalkboard-teacher"></i> Enseignements</a>
-                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/delegue.php"?>"><i class="fas fa-vote-yea"></i> Délégué</a>
-                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/conferences.php"?>"><i class="fas fa-book-reader"></i> Conférences</a> <!--à garder ou à changer ?-->
-                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/publications.php"?>"><i class="fas fa-book"></i> Publications</a>
+                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/enseignements.php" ?>"><i class="fas fa-chalkboard-teacher"></i> Enseignements</a>
+                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/delegue.php" ?>"><i class="fas fa-vote-yea"></i> Délégué</a>
+                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/conferences.php" ?>"><i class="fas fa-book-reader"></i> Conférences</a>
+                    <a class="dropdown-item" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/publications.php" ?>"><i class="fas fa-book"></i> Publications</a>
                 </div>
             </li>
             <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="<?php echo $currPage?>" id="langages" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="flag-icon flag-icon-fr"> </span>  Français</a>
+                <a class="nav-link dropdown-toggle" href="<?php echo $currPage ?>" id="langages" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="flag-icon flag-icon-fr"></span> Français</a>
                 <div class="dropdown-menu" aria-labelledby="langages">
-                    <a class="dropdown-item" href="<?php echo $newPageEn?>"><span class="flag-icon flag-icon-gb"> </span> English</a>
-                    <a class="dropdown-item" href="<?php echo $newPageIt?>"><span class="flag-icon flag-icon-it"> </span> Italiano</a>
+                    <a class="dropdown-item" href="<?php echo $newPageEn ?>"><span class="flag-icon flag-icon-gb"> </span> English</a>
+                    <a class="dropdown-item" href="<?php echo $newPageIt ?>"><span class="flag-icon flag-icon-it"> </span> Italiano</a>
                 </div>
             </li>
-            <!-- <li class="nav-item">
-                <a class="navbar-brand" href="<?php echo $_SESSION['baseURL'] . $_SESSION['language'] . "/test.php"?>" style="color:lightblue">Test</a> 
-            </li> -->
         </ul>
     </div>
 </nav>
